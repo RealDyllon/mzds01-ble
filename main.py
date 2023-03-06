3b5101a21344b3540828737846c6f7bf8a9cf7e0
Dyllon <dyllon.g@outlook.com>
2023-03-06 19:58:05 +0800

    update notification_handler

--- main.py
+++ main.py
@@ -3,14 +3,85 @@
 
 import re
 import sys
+import json
+import enum
 import asyncio
 import argparse
+from binascii import hexlify
 from typing import Dict, Any, List, Callable, Optional
 
 from bleak import BleakScanner, BleakClient
 from bleak.backends.device import BLEDevice as BleakDevice
 
 from logger import logger
+
+
+class Response:
+    def __init__(self) -> None:
+        self.bytes_remaining = 0
+        self.bytes = bytearray()
+        self.data: Dict[int, bytes] = {}
+        self.id: int
+        self.status: int
+
+    def __str__(self) -> str:
+        return json.dumps(self.data, indent=4, default=lambda x: x.hex(":"))
+
+    @property
+    def is_received(self) -> bool:
+        return len(self.bytes) > 0 and self.bytes_remaining == 0
+
+    def accumulate(self, data: bytes) -> None:
+        CONT_MASK = 0b10000000
+        HDR_MASK = 0b01100000
+        GEN_LEN_MASK = 0b00011111
+        EXT_13_BYTE0_MASK = 0b00011111
+
+        class Header(enum.Enum):
+            GENERAL = 0b00
+            EXT_13 = 0b01
+            EXT_16 = 0b10
+            RESERVED = 0b11
+
+        buf = bytearray(data)
+        if buf[0] & CONT_MASK:
+            buf.pop(0)
+        else:
+            # This is a new packet so start with an empty byte array
+            self.bytes = bytearray()
+            hdr = Header((buf[0] & HDR_MASK) >> 5)
+            if hdr is Header.GENERAL:
+                self.bytes_remaining = buf[0] & GEN_LEN_MASK
+                buf = buf[1:]
+            elif hdr is Header.EXT_13:
+                self.bytes_remaining = ((buf[0] & EXT_13_BYTE0_MASK) << 8) + buf[1]
+                buf = buf[2:]
+            elif hdr is Header.EXT_16:
+                self.bytes_remaining = (buf[1] << 8) + buf[2]
+                buf = buf[3:]
+
+        # Append payload to buffer and update remaining / complete
+        self.bytes.extend(buf)
+        self.bytes_remaining -= len(buf)
+        logger.info(f"{self.bytes_remaining=}")
+
+    def parse(self) -> None:
+        self.id = self.bytes[0]
+        self.status = self.bytes[1]
+        buf = self.bytes[2:]
+        while len(buf) > 0:
+            # Get ID and Length
+            param_id = buf[0]
+            param_len = buf[1]
+            buf = buf[2:]
+            # Get the value
+            value = buf[:param_len]
+
+            # Store in dict for later access
+            self.data[param_id] = value
+
+            # Advance the buffer
+            buf = buf[param_len:]
 
 
 def exception_handler(loop: asyncio.AbstractEventLoop, context: Dict[str, Any]) -> None:
@@ -109,12 +180,52 @@ async def connect_ble(
 
 
 async def main(identifier: Optional[str]) -> None:
-    def dummy_notification_handler(*_: Any) -> None:
-        ...
-    # find and connect to client
-    client = await connect_ble(dummy_notification_handler, identifier)
 
-    #
+    event = asyncio.Event()
+
+    # UUIDs to write to and receive responses from
+    QUERY_REQ_UUID = "10e2fde2-d7fe-4845-b3f3-a32010ebb095"
+    # QUERY_RSP_UUID = "smth"
+
+    #     "fa879af4-d601-420c-b2b4-07ffb528dde3",
+    #     "0000ae00-0000-1000-8000-00805f9b34fb"
+    response_uuid = "fa879af4-d601-420c-b2b4-07ffb528dde3" # QUERY_RSP_UUID
+
+    client: BleakClient
+    response = Response()
+
+    def notification_handler(handle: int, data: bytes) -> None:
+        logger.info(f'Received response at {handle=}: {hexlify(data, ":")!r}')
+
+        response.accumulate(data)
+
+        if response.is_received:
+            response.parse()
+
+            # If this is the correct handle and the status is success, the command was a success
+            if client.services.characteristics[handle].uuid == response_uuid and response.status == 0:
+                logger.info("Successfully received the response")
+            # Anything else is unexpected. This shouldn't happen
+            else:
+                logger.error("Unexpected response")
+
+            # Notify writer that procedure is complete
+            event.set()
+
+    # find and connect to client
+    client = await connect_ble(notification_handler, identifier)
+
+    logger.info("Client services:")
+    for service in client.services:
+        logger.info(f"{service.uuid} - ({len(service.characteristics)})")
+
+    # get characteristics
+    logger.info("Getting the device's settings...")
+    event.clear()
+    # await client.write_gatt_char(QUERY_REQ_UUID, bytearray([0x01, 0x12]), response=True)
+    await client.read_gatt_char("Reading is not permitted")
+    await event.wait()  # Wait to receive the notification response
+    logger.info(f"Received settings\n: {response}")
 
     await client.disconnect()
 
